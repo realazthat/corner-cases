0232b801696399361d1f00e991a4e397b66d621d
realazthat <realazthat@gmail.com>
2015-12-22 01:54:01 -0500

    Added install scripts, mainly for googletest, meant for travis ci

---
+++ scripts/install-deps-via-apt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#####################################################################
+####Run this with sudo
+#####################################################################
+
+
+#exit on failed line
+set -exv
+
+apt-get update
+apt-get install libgtest-dev
+
+
