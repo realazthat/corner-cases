ecd1888082cf6ed6b29a7ebed9e8c6bfd954f829
realazthat <realazthat@gmail.com>
2016-01-19 14:48:25 -0500

    Forward declaring tostr() in the headers for cubexx formatters

--- include/cubexx/formatters.hpp
+++ include/cubexx/formatters.hpp
@@ -11,10 +11,15 @@
 
 
 namespace cubexx{
-::std::ostream& operator<<(::std::ostream& out, const face_t& face);
-::std::ostream& operator<<(::std::ostream& out, const direction_t& direction);
 ::std::ostream& operator<<(::std::ostream& out, const corner_t& corner);
+::std::ostream& operator<<(::std::ostream& out, const direction_t& direction);
 ::std::ostream& operator<<(::std::ostream& out, const edge_t& edge);
+::std::ostream& operator<<(::std::ostream& out, const face_t& face);
+
+::std::string tostr(const corner_t& corner);
+::std::string tostr(const direction_t& direction);
+::std::string tostr(const edge_t& edge);
+::std::string tostr(const face_t& face);
 
 } // namespace cubexx
 
