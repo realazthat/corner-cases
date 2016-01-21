964face8a9d0038b7743743e55ccada4781059a5
realazthat <realazthat@gmail.com>
2016-01-20 19:02:19 -0500

    Added/fixed cubexx unit tests
    - Added cubexx unit test corner_opposite
    - Fixed cubexx unit test corner_adjacent_direction
    - Added cubexx unit test corner_push_direction

--- src/unittests/cubexx.cpp
+++ src/unittests/cubexx.cpp
@@ -77,6 +77,48 @@ TEST_F(CUBEXXTest,corner_unique)
     }
 }
 
+TEST_F(CUBEXXTest,corner_opposite)
+{
+
+    ///test corner_t::opposite_corner()
+    {
+        
+        
+        for (auto corner : cubexx::corner_t::all())
+        {
+            
+            auto opposite_corner = corner.opposite();
+            
+            
+            EXPECT_NE(corner, opposite_corner);
+            EXPECT_EQ(corner, opposite_corner.opposite());
+            EXPECT_EQ(-opposite_corner.x(), corner.x());
+            EXPECT_EQ(-opposite_corner.y(), corner.y());
+            EXPECT_EQ(-opposite_corner.z(), corner.z());
+            
+        }
+        
+    }
+}
+
+
+TEST_F(CUBEXXTest,corner_index)
+{
+
+
+    ///test corner_t::index() and corner_t::index(index)
+    uint32_t index = 0;
+    for (auto corner : cubexx::corner_t::all())
+    {
+        EXPECT_EQ(index, corner.index());
+        
+        EXPECT_EQ(corner, cubexx::corner_t::index(index));
+        
+        ++index;
+    }
+}
+
+
 TEST_F(CUBEXXTest,corner_get)
 {
 
@@ -114,43 +156,110 @@ TEST_F(CUBEXXTest,corner_adjacent_direction)
             {
                 auto corner1 = corner0.adjacent(direction);
                 
-                EXPECT_NE(corner0, corner1);
+                ASSERT_NE(corner0, corner1) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
                 
                 
-                EXPECT_TRUE(corner0.is_adjacent(corner1));
+                ASSERT_TRUE(corner0.is_adjacent(corner1)) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
                 
                 
+                bool expected_out_of_bounds = false;
                 int xyz0[] = {corner0.x(), corner0.y(), corner0.z()};
                 int dir[] = {direction.x(), direction.y(), direction.z()};
                 int xyz1_expected[] = {0,0,0};
-                for (int i = 0; i < 3; ++i)
+                for (int i = 0; i < 3; ++i) {
+                    ///add the direction in
                     xyz1_expected[i] = xyz0[i] + dir[i]*2;
-                
-                bool expected_out_of_bounds = false;
-                
-                for (int i = 0; i < 3; ++i)
-                    if (std::abs(xyz1_expected[i]) != 1)
+                    ///if it is out of bounds, move it around and back to the opposite side
+                    if (xyz1_expected[i] > 1) {
+                        xyz1_expected[i] = -1;
                         expected_out_of_bounds = true;
+                    } else if (xyz1_expected[i] < -1) {
+                        xyz1_expected[i] = 1;
+                        expected_out_of_bounds = true;
+                    } else {
+                        
+                    }
+                }
                 
                 
-                //ASSERT_TRUE(!is_null_corner(corner1) || expected_out_of_bounds) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
                 
                 
-                //if (is_null_corner(corner1))
-                //    continue;
-                    
                 int xyz1_actual[] = {corner1.x(), corner1.y(), corner1.z()};
                 
                 for (int i = 0; i < 3; ++i)
-                    EXPECT_EQ(xyz1_actual[i] - xyz0[i], 2*dir[i]) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
+                    ASSERT_EQ(std::abs(xyz1_actual[i] - xyz0[i]), std::abs(2*dir[i]))
+                            << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1
+                            << ", i: " << i;
                 for (int i = 0; i < 3; ++i)
-                    EXPECT_EQ(xyz1_actual[i], xyz1_expected[i]) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
+                    ASSERT_EQ(xyz1_expected[i], xyz1_actual[i])
+                            << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1
+                            << ", i: " << i;
                 
             }
         }
-        
-        
+    }
+}
 
+
+TEST_F(CUBEXXTest,corner_push_direction)
+{
+
+    ///test corner push(direction_t)
+    {
+        
+        for (auto corner0 : cubexx::corner_t::all())
+        {
+            for (auto direction : cubexx::direction_t::all())
+            {
+                int xyz0[] = {corner0.x(),corner0.y(), corner0.z()};
+                int dir[] = {direction.x(),direction.y(), direction.z()};
+                int xyz1_expected[] = {0,0,0};
+                
+                bool expected_out_of_bounds = false;
+                for (int i = 0; i < 3; ++i)
+                {
+                    xyz1_expected[i] = xyz0[i] + 2*dir[i];
+                    
+                    if (xyz1_expected[i] > 1)
+                    {
+                        xyz1_expected[i] = 1;
+                        expected_out_of_bounds = true;
+                    } else if (xyz1_expected[i] < -1)
+                    {
+                        xyz1_expected[i] = -1;
+                        expected_out_of_bounds = true;
+                    }
+                }
+                
+                
+                auto corner1 = corner0.push(direction);
+                int xyz1_actual[] = {corner1.x(),corner1.y(), corner1.z()};
+                
+                
+                for (int i = 0; i < 3; ++i)
+                    ASSERT_EQ(xyz1_expected[i], xyz1_actual[i])
+                            << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1
+                            << ", i: " << i;
+                
+                if (corner0 == corner1)
+                {
+                    
+                    
+                    continue;
+                }
+                
+                
+                ASSERT_TRUE(corner0.is_adjacent(corner1));
+                
+                
+                
+                for (int i = 0; i < 3; ++i)
+                    ASSERT_EQ(xyz1_actual[i] - xyz0[i], 2*dir[i]) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
+                for (int i = 0; i < 3; ++i)
+                    ASSERT_EQ(xyz1_actual[i], xyz1_expected[i]) << "corner0: " << corner0 << ", direction: " << direction << ", corner1: " << corner1;
+                
+            }
+        }
     }
 }
 
