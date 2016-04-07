aa189d2acfa52d1e53d8382daf53ab0332147d89
realazthat <realazthat@gmail.com>
2016-04-07 00:54:51 -0500

    cubexx: fixed unit test CUBEXXDirectionTest::axis

--- src/unittests/cubexx-direction.cpp
+++ src/unittests/cubexx-direction.cpp
@@ -240,15 +240,15 @@ TEST_F(CUBEXXDirectionTest,axis)
         auto axis = direction.axis();
         auto xyz = direction.xyz();
 
-        ASSERT_NE(0U, direction.xyz()[axis]);
+        ASSERT_NE(0, direction.xyz()[axis]);
 
-        ASSERT_EQ(0U, direction.xyz()[(axis + 1 ) % 3]) << "direction: " << direction
-                                                        << ", axis: " << (int)axis
-                                                        << ", (axis + 1 ) % 3: " << (int)((axis + 1 ) % 3)
-                                                        << ", direction.xyz()[(axis + 1 ) % 3]: " << (int)direction.xyz()[(axis + 1 ) % 3]
-                                                        << ", xyz[(axis + 1 ) % 3]: " << (int)xyz[(axis + 1 ) % 3]
+        ASSERT_EQ(0, direction.xyz()[(axis + 1 ) % 3]) << "direction: " << direction
+                                                       << ", axis: " << (int)axis
+                                                       << ", (axis + 1 ) % 3: " << (int)((axis + 1 ) % 3)
+                                                       << ", direction.xyz()[(axis + 1 ) % 3]: " << (int)direction.xyz()[(axis + 1 ) % 3]
+                                                       << ", xyz[(axis + 1 ) % 3]: " << (int)xyz[(axis + 1 ) % 3]
                                                         ;
-        ASSERT_EQ(0U, direction.xyz()[(axis + 2 ) % 3]) << "direction: " << direction << ", axis: " << (int)axis;
+        ASSERT_EQ(0, direction.xyz()[(axis + 2 ) % 3]) << "direction: " << direction << ", axis: " << (int)axis;
     }
 }
 
